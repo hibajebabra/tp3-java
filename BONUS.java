07cba7c29330e61c66e80715cc3757fa04f5cae0
Hiba diba <jebabrahiba161@gmail.com>
2026-04-14 20:41:11 +0100

    Update BONUS.java

--- BONUS.java
+++ BONUS.java
@@ -6,39 +6,39 @@ import java.util.Scanner;
 public class BONUS{
     public static void main(String[] args) {
         
-        // Générer un nombre aléatoire entre 1 et 10
+        
         Random rand = new Random();
         int nombreSecret = rand.nextInt(10) + 1;
         
-        // Scanner pour lire les entrées utilisateur
+        
         Scanner sc = new Scanner(System.in);
         
         int tentative = 0;
         int nombreSaisi = 0;
         
-        System.out.println("🎯 Devinez un nombre entre 1 et 10 !");
+        System.out.println(" Devinez un nombre entre 1 et 10 !");
         
-        // Boucle jusqu'à ce que l'utilisateur trouve
+        
         while (nombreSaisi != nombreSecret) {
             System.out.print("Votre proposition : ");
             nombreSaisi = sc.nextInt();
             
-            tentative++; // compter les essais
+            tentative++; 
             
             if (nombreSaisi < nombreSecret) {
-                System.out.println("➡️ Plus grand");
+                System.out.println(" Plus grand");
             } 
             else if (nombreSaisi > nombreSecret) {
-                System.out.println("⬅️ Plus petit");
+                System.out.println(" Plus petit");
             } 
             else {
-                System.out.println("🎉 Bravo !");
+                System.out.println(" Bravo !");
             }
         }
         
-        // Affichage du score
-        System.out.println("✅ Vous avez trouvé en " + tentative + " tentative(s).");
+        
+        System.out.println(" Vous avez trouvé en " + tentative + " tentative(s).");
         
         sc.close();
     }
-}
+} 
